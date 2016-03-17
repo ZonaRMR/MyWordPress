6c96522dbea4753af412af478bd17e667da2b458
Keyi,Shui <329067804@qq.com>
2016-03-17 14:42:41 +0800

    Add Adapter for recycler

---
+++ app/src/main/java/cn/suiseiseki/www/suiseiseeker/model/CategoryAdapter.java
@@ -0,0 +1,39 @@
+package cn.suiseiseki.www.suiseiseeker.model;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Suiseiseki/shuikeyi on 2016/3/17.
+ * Use FragmentPagerAdapter instead of FragmentStatePagerAdapter to secure safety,as there are not so many categories
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+    private ArrayList<Category> mCategories;
+    public CategoryAdapter(FragmentManager fm,ArrayList<Category> categories)
+    {
+        super(fm);
+        mCategories = categories;
+    }
+    public int getCount()
+    {
+        return mCategories.size();
+    }
+    public Fragment getItem(int pos)
+    {
+        return null;
+    }
+    /**
+     * Set the Title of Page
+     */
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        return mCategories.get(position).getName();
+    }
+
+
+}
